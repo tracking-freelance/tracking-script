ef124470b9f493d58c775b2053edede8707d6faa
Pham Thanh Trung <adamward459@gmail.com>
2023-03-04 20:47:01 +0700

    add env

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
 // vite.config.js
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/tracking.ts"),
-      name: "tracking",
-      fileName: "tracking"
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/tracking.ts"),
+        name: "tracking",
+        fileName: "tracking"
+      },
+      rollupOptions: {
+        treeshake: true
+      },
+      minify: true
     },
-    rollupOptions: {
-      treeshake: true
-    },
-    minify: true
-  }
+    define: {
+      VITE_SERVER: env.VITE_SERVER
+    }
+  };
 });
