1326cfb88f97197f9c2f7371435b85c868e3b600
Pham Thanh Trung <adamward459@gmail.com>
2023-03-04 20:49:06 +0700

    add env for build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-
+  console.log(env);
   return {
     build: {
       lib: {
