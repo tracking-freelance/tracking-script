59f785180169e27b497b63b6eaf3947b03ea58f9
Pham Thanh Trung <adamward459@gmail.com>
2023-03-13 21:16:17 +0700

    add trycatch

--- src/tracking.ts
+++ src/tracking.ts
@@ -1,32 +1,37 @@
 import { record } from "rrweb";
 
-
 const UserLocalStorageIdKey = "tracking_user_id";
 
 (() => {
-  record({
-    emit(event) {
-      const userId = localStorage.getItem(UserLocalStorageIdKey)
-      if (userId) {
-        fetch(import.meta.env.VITE_SERVER + "/track", {
-          method: "POST",
-          body: JSON.stringify({ userId: parseInt(userId), ...event })
-        }).catch(console.error)
-      } else {
-        fetch(import.meta.env.VITE_SERVER + "/track/init", {
-          method: "POST"
-        }).then(res => {
-          return res.json()
-        }).then(res => {
-          localStorage.setItem(UserLocalStorageIdKey, res.userId)
-        }).catch(console.error)
+  try {
+    record({
+      emit(event) {
+        const userId = localStorage.getItem(UserLocalStorageIdKey);
+        if (userId) {
+          fetch(import.meta.env.VITE_SERVER + "/track", {
+            method: "POST",
+            body: JSON.stringify({ userId: parseInt(userId), ...event })
+          }).catch(console.error);
+        } else {
+          fetch(import.meta.env.VITE_SERVER + "/track/init", {
+            method: "POST"
+          })
+            .then((res) => {
+              return res.json();
+            })
+            .then((res) => {
+              localStorage.setItem(UserLocalStorageIdKey, res.userId);
+            })
+            .catch(console.error);
+        }
+      },
+      sampling: {
+        mousemove: true,
+        scroll: 0,
+        mouseInteraction: true
       }
-    },
-    sampling: {
-      mousemove: true,
-      scroll: 0,
-      mouseInteraction: true,
-    },
-  })
-})()
-
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
