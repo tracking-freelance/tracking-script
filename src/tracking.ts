0b0fd6398926c1ee15db8d84a099901d25cdf319
Phạm Thành Trung <adamward459@gmail.com>
2023-05-17 23:10:47 +0700

    update script

--- src/tracking.ts
+++ src/tracking.ts
@@ -1,41 +1,26 @@
 import { record } from "rrweb";
 
-const UserLocalStorageIdKey = "tracking_user_id";
+let events: any[] = [];
 
-(() => {
-  try {
-    record({
-      emit(event) {
-        const userId = localStorage.getItem(UserLocalStorageIdKey);
-        if (userId) {
-          fetch(import.meta.env.VITE_SERVER + "/trackings", {
-            method: "POST",
-            body: JSON.stringify({ ...event }),
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json"
-            }
-          }).catch(console.error);
-        } else {
-          fetch(import.meta.env.VITE_SERVER + "/track/init", {
-            method: "POST"
-          })
-            .then((res) => {
-              return res.json();
-            })
-            .then((res) => {
-              localStorage.setItem(UserLocalStorageIdKey, res.userId);
-            })
-            .catch(console.error);
-        }
-      },
-      sampling: {
-        mousemove: true,
-        scroll: 0,
-        mouseInteraction: true
-      }
-    });
-  } catch (error) {
-    console.error(error);
+record({
+  emit(event) {
+    events.push(event);
   }
-})();
+});
+
+async function save() {
+  const body = JSON.stringify({ events });
+  events = [];
+  await fetch(import.meta.env.VITE_SERVER + "/trackings", {
+    method: "POST",
+    body,
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+setInterval(save, 5000);
+
+window.addEventListener("beforeunload", save);
