17b133ab0765047c0f9fcca78448031ee2b65344
Phạm Thành Trung <adamward459@gmail.com>
2023-05-20 19:10:16 +0700

    end session

--- src/tracking.ts
+++ src/tracking.ts
@@ -24,13 +24,12 @@ async function save() {
 setInterval(save, 5000);
 
 window.addEventListener("beforeunload", () => {
-  save().then(async () => {
-    await fetch(import.meta.env.VITE_SERVER + "/trackings/end", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
+  save().then(async () => {});
+  fetch(import.meta.env.VITE_SERVER + "/trackings/end", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    }
   });
 });
