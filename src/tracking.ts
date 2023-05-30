1a52ca374fcd4e66aff7b629af70110c6995aff0
Phạm Thành Trung <adamward459@gmail.com>
2023-05-30 19:29:11 +0700

    remove end script

--- src/tracking.ts
+++ src/tracking.ts
@@ -23,20 +23,20 @@ async function save() {
 
 setInterval(save, 5000);
 
-window.addEventListener("beforeunload", (e) => {
-  // e = e || window.event;
-  // // For IE and Firefox prior to version 4
-  // if (e) {
-  //   e.returnValue = "Sure?";
-  // }
-  // save().then(async () => {});
-  // fetch(import.meta.env.VITE_SERVER + "/trackings/end", {
-  //   method: "POST",
-  //   credentials: "include",
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   }
-  // });
-  // // For Safari
-  // return "Sure?";
-});
+// window.addEventListener("beforeunload", (e) => {
+//   // e = e || window.event;
+//   // // For IE and Firefox prior to version 4
+//   // if (e) {
+//   //   e.returnValue = "Sure?";
+//   // }
+//   // save().then(async () => {});
+//   // fetch(import.meta.env.VITE_SERVER + "/trackings/end", {
+//   //   method: "POST",
+//   //   credentials: "include",
+//   //   headers: {
+//   //     "Content-Type": "application/json"
+//   //   }
+//   // });
+//   // // For Safari
+//   // return "Sure?";
+// });
